d48262a8608081439e952e1007d81aded2b02e71
Samxzeng2019 <38000311+Samxzeng@users.noreply.github.com>
2020-05-16 23:15:49 +0800

    q16 and typo changes

--- zengxin/mytools/tools.py
+++ zengxin/mytools/tools.py
@@ -1,4 +1,4 @@
-#!/usr/bin/evn python
+#!/usr/bin/env python
 
 def read_file(type):
     with open('input') as f:
@@ -9,3 +9,21 @@ def read_file(type):
             mystring = f.read()
     f.close()
     return mylist if mylist else mystring
+
+'''
+#!/usr/bin/env python
+import sys
+sys.path.append("..")
+from mytools import tools
+
+start_list = tools.read_file("line")
+
+
+def main(input):
+
+    result = 0
+    print(result)
+    return(result)
+
+main(start_list)
+'''
