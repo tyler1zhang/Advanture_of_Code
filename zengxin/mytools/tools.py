432599bcf512b8996bf40905b66afba04ed01b09
Samxzeng2019 <38000311+Samxzeng@users.noreply.github.com>
2020-05-17 22:28:24 +0800

    q17 done

--- zengxin/mytools/tools.py
+++ zengxin/mytools/tools.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 
 def read_file(type):
     with open('input') as f:
@@ -9,21 +8,3 @@ def read_file(type):
             mystring = f.read()
     f.close()
     return mylist if mylist else mystring
-
-'''
-#!/usr/bin/env python
-import sys
-sys.path.append("..")
-from mytools import tools
-
-start_list = tools.read_file("line")
-
-
-def main(input):
-
-    result = 0
-    print(result)
-    return(result)
-
-main(start_list)
-'''
