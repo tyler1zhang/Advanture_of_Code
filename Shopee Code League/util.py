4389996e466a90d9aeb29fedeca1a4a061586901
RyanGuo <ruiliang.guo@gmail.com>
2020-06-13 13:43:49 +0800

    add csv read function

--- Shopee Code League/util.py
+++ Shopee Code League/util.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 import csv
 
+#### to read data from csv file####
+def read_csv(file):
+    with open(file) as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        print(f'field names are {csv_reader.fieldnames}')
+        for row in csv_reader:
+            print(row.values())
+    
 #### to write data to csv file####
 def write_csv(file, fieldnames, data):
     
@@ -11,6 +19,8 @@ def write_csv(file, fieldnames, data):
         for row in data:
             writer.writerow(row)
 
+
+### Testing ###  
 file='GTZZ_Team.csv'
 fieldnames = ['index', 'name', 'sex']        
 data=[{'index': 1, 'name': 'Zeng Xin', 'sex': 'M'},
@@ -18,4 +28,5 @@ data=[{'index': 1, 'name': 'Zeng Xin', 'sex': 'M'},
       {'index': 3, 'name': 'Tao Sui', 'sex': 'M'},
       {'index': 4, 'name': 'Guo Ruiliang', 'sex': 'M'},
       ]
-print(write_csv(file, fieldnames, data))
+#write_csv(file, fieldnames, data)
+read_csv(file)
