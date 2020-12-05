3fc47f701469c3a69eb30ae3e12d0e18204fd669
Samxzeng2019 <53826905+Samxzeng2019@users.noreply.github.com>
2020-12-05 22:50:49 +0800

    done day2

--- zengxin2020/logic.py
+++ zengxin2020/logic.py
@@ -28,6 +28,33 @@ def day1part2():
             return functools.reduce(lambda a, b: a * b, el)
 
 
+def day2part1():
+    password_list = read_file("2", "list")
+    valid_password_counter = 0
+    for password in password_list:
+        policy_limit = re.findall(r'\d+', password)
+        policy_word = re.search(r'\w:', password).group()[0]
+        password_str = password.split()[-1]
+        repeat_num = password_str.count(policy_word)
+        if repeat_num <= int(policy_limit[1]) and repeat_num >= int(policy_limit[0]):
+            valid_password_counter += 1
+    return valid_password_counter
+
+
+def day2part2():
+    password_list = read_file("2", "list")
+    valid_password_counter = 0
+    for password in password_list:
+        policy_limit = re.findall(r'\d+', password)
+        policy_word = re.search(r'\w:', password).group()[0]
+        password_str = password.split()[-1]
+        if password_str[int(policy_limit[0]) - 1] == policy_word and password_str[int(policy_limit[1]) - 1] != policy_word:
+            valid_password_counter += 1
+        elif password_str[int(policy_limit[0]) - 1] != policy_word and password_str[int(policy_limit[1]) - 1] == policy_word:
+            valid_password_counter += 1
+    return valid_password_counter
+
+
 if __name__ == "__main__":
-    print(day1part1())
-    print(day1part2())
+    print(day2part2())
+    # print(day1part2())
